55e2cf245f3093c80b6b8b820fc48a95c76eb348
lagijk <achen119@bu.edu>
2025-02-02 15:55:41 -0500

    added exponent function into the js calculator

--- projects.js
+++ projects.js
@@ -78,3 +78,29 @@ function division() {
         output.style.color = "";
     }
 }
+
+// exponent operator
+function exponent() {
+    // convert two input into ints 
+    const num1 = Number(firstNum.value);
+    const num2 = Number(secondNum.value);
+
+    //calculate and display result of power operator using for loop
+    let result = 1; //set result to 1 for 0 exponent
+    //use Math.abs() to make sure negative exponent can be calculated
+    for (i = 0; i < Math.abs(num2); i++) {
+        result *= num1;
+    }
+    // calculate reciprocal for negative exponent
+    if (num2 < 0) {
+        result = 1 / result;
+    }
+    output.innerHTML=String(result);
+
+    // if result is negative change text color to red
+    if (result < 0) {
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
+    }
+}
