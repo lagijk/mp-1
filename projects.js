d1270f42936de6f5edbe576f613836b6ae9f72b1
lagijk <achen119@bu.edu>
2025-02-02 15:42:21 -0500

    added global variable for clarity, added sub, mul and div operators

--- projects.js
+++ projects.js
@@ -1,23 +1,80 @@
-// get the value of two inputs
-//const firstNum = document.getElementById("first-number").value;
-//const secondNum = document.getElementById("second-number").value;
-//const output = document.getElementById("output");
+// global variable of two inputs
+const firstNum = document.getElementById("first-number");
+const secondNum = document.getElementById("second-number");
+// global variable of output
+const output = document.getElementById("output");
 
 // addition operation
 function addition() {
-    // get the value of two inputs
-    const firstNum = document.getElementById("first-number").value;
-    const secondNum = document.getElementById("second-number").value;
     // convert two input into ints 
-    const num1 = Number(firstNum);
-    const num2 = Number(secondNum);
+    const num1 = Number(firstNum.value);
+    const num2 = Number(secondNum.value);
+    //calculate and display result
     const result = num1 + num2;
-    document.getElementById("output").innerHTML=String(result);
+    output.innerHTML=String(result);
 
     // if result is negative change text color to red
     if (result < 0) {
-        document.getElementById("output").style.color = "red";
+        output.style.color = "red";
     } else {
-        document.getElementById("output").style.color = "";
+        output.style.color = "";
+    }
+}
+
+// subtraction operator
+function subtraction() {
+    // convert two input into ints 
+    const num1 = Number(firstNum.value);
+    const num2 = Number(secondNum.value);
+    //calculate and display result
+    const result = num1 - num2;
+    output.innerHTML=String(result);
+
+    // if result is negative change text color to red
+    if (result < 0) {
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
+    }
+}
+
+// multiplication operator
+function multiply() {
+    // convert two input into ints 
+    const num1 = Number(firstNum.value);
+    const num2 = Number(secondNum.value);
+    //calculate and display result
+    const result = num1 * num2;
+    output.innerHTML=String(result);
+
+    // if result is negative change text color to red
+    if (result < 0) {
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
+    }
+}
+
+// division operator
+function division() {
+    // convert two input into ints 
+    const num1 = Number(firstNum.value);
+    const num2 = Number(secondNum.value);
+
+    // if second number is 0 show division by 0 error
+    if (num2 === 0) {
+        output.innerHTML = "Error: Division By Zero";
+        return;
+    }
+
+    //calculate and display result
+    const result = num1 / num2;
+    output.innerHTML=String(result);
+
+    // if result is negative change text color to red
+    if (result < 0) {
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
     }
 }
